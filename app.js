9f603bcb5fd019f19b97a034335879b4011ac746
emilymay200 <emilycryer21@gmail.com>
2020-09-27 16:21:25 +0100

    BoldFunction
    Upon pressing analyse the word "lorem" will bold

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-window.onload = function() {
 
-    x = document.getElementById("exPara").innerHTML;
-    console.log(exPara);
-
+function analyse() {
+    var x = document.getElementById("exPara").innerHTML;
+    console.log(x);
+    x1 = x.replace(/Lorem/gi, "<b> Lorem </b>");
+    console.log(x1);
+    document.getElementById("exPara").innerHTML = x1;
 }

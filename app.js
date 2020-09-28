5457ca2162219bb1e8e14f7468cbb43112191b1b
daniela-d-d <daniela.d.d@outlook.com>
2020-09-28 14:07:18 +0100

    removed unnecessary files

--- app.js
+++
@@ -1,8 +0,0 @@
-
-function analyse() {
-    var x = document.getElementById("exPara").innerHTML;
-    console.log(x);
-    x1 = x.replace(/Lorem/gi, "<b> Lorem </b>");
-    console.log(x1);
-    document.getElementById("exPara").innerHTML = x1;
-}
